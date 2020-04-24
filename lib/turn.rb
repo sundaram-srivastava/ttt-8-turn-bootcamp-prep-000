0c0acf6f979d2510cc797ed646564ecd281d0937
Sundaram Srivastava <sundaram.srivastav007@gmail.com>
2020-04-24 18:13:18 +0000

    Automatically backed up by Learn

---
+++ lib/turn.rb
@@ -0,0 +1,16 @@
+def board(number)
+  if number==1
+      puts "| X |  |  |
+------------
+|  |  |  |
+------------
+|  |  |  |"
+  end
+
+end
+
+board(1)
+def turn(number)
+  puts "Please enter 1-9:"
+end
+turn(number)
